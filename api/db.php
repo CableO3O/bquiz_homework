1531b82a1d5032e08025cd697a0fc0a3a0e01914
cable <cable870526@gmail.com>
2024-01-02 08:31:25 +0800

    改資料庫名稱

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 class DB{
 
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db22";
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=bquiz_01";
     protected $pdo;
     protected $table;
 
